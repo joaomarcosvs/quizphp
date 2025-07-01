88909253a3b36a81ee5d478f6e4f5f14853163bd
João Marcos <joaomarcos.vieirasantos@gmail.com>
2025-07-01 14:05:29 -0300

    update

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,14 @@ use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 //start game
+// Rota de get 1.nome da rota 2.de onde vem o metodo 3.nome do metodo 4.nome da rota
 Route::get('/', [MainController::class, 'startGame'])->name('start_game');
 Route::post('/', [MainController::class, 'prepareGame'])->name('prepare_game');
 
 //in game
 Route::get('/game', [MainController::class, 'game'])->name('game');
 Route::get('/answer/{answer}', [MainController::class, 'answer'])->name('answer');
+Route::get('/next_question', [MainController::class, 'nextQuestion'])->name('next_question');
+
+//game over
+Route::get('/show_results', [MainController::class, 'showResults'])->name('show_results');
